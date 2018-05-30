e465aabf8c317627b6a79b339b15dfcfa31b9ffa
bingli <36870855@qq.com>
2018-05-30 16:56:26 +0800

    Gradle添加RxBinding依赖

--- app/src/main/java/com/example/kadh/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/kadh/ui/login/LoginActivity.java
@@ -1,16 +1,7 @@
 package com.example.kadh.ui.login;
 
-import android.util.Log;
-
 import com.example.kadh.R;
 import com.example.kadh.base.BaseActivity;
-import com.example.kadh.utils.RxJava.BaseResponse;
-import com.example.kadh.utils.RxJava.RxSubscriber.SubDialog;
-import com.example.kadh.utils.RxJava.RxApi.RxApiManager;
-import com.example.kadh.utils.RxJava.RxSubscriber.SubProgress;
-import com.example.kadh.utils.RxJava.RxSubscriber.SubOnNextImpl;
-
-import java.util.List;
 
 /**
  * @author: kadh
@@ -29,13 +20,7 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void initDatas() {
-        SubOnNextImpl<BaseResponse<List<LoginModel>>> loginListener = new SubOnNextImpl<BaseResponse<List<LoginModel>>>() {
-            @Override
-            public void onSubSuccess(BaseResponse<List<LoginModel>> baseResponse) {
-                Log.d(TAG, "baseResponse:" + baseResponse.data);
-            }
-        };
-        RxApiManager.getRxApi().login(new SubProgress<BaseResponse<List<LoginModel>>>(loginListener, this, SubDialog.NODIALOG), "1000047", "123456");
+
     }
 
     @Override
